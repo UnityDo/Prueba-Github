bfa18d7d5fb3e4dd2338736dc968518670332d62
UnityDo <jorgevallejodecastro@gmail.com>
2023-06-27 13:30:06 +0200

    Subida
    Jorge // Dev

---
+++ Source/PrubeaGithub/PruebaSubida.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "PruebaSubida.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRUBEAGITHUB_API UPruebaSubida : public UObject
+{
+	GENERATED_BODY()
+	
+};
